790ac156c5808ee1d3147340200574f2e684c9ad
George Province <provincegeorge412@gmail.com>
2023-08-12 02:05:37 +0530

    some more commands

--- DMLcommands.sql
+++ DMLcommands.sql
@@ -16,3 +16,21 @@ update customer set city = 'hyderabad', gender = 'M',pincode = 211002 where id =
 
 delete from customer where id = 121;
 select * from customer;
+
+insert into customer(id,cname,city,gender,pincode) values
+(10,'Prajjwal','Prayagraj','M', 211006),
+(11,'Srishti','allapur','F', 211007),
+(12,'Pranjal','Prayagraj','F', 211006),
+(13,'Divya prakash','daraganj','M', 211003),
+(14,'pinky','Prayagraj','F', 211006),
+(15,'mayank tripathi','daraganj','M', 211003);
+
+select * from customer;
+-- update multiple rows 
+-- to turn safe mode off 
+set sql_safe_updates = 0;
+update customer set pincode = 211005;
+update customer set pincode = pincode + 1;
+
+delete from customer where id = 10;
+delete from customer;
